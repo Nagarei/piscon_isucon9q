561828830b7c0474a6695852dad52c3df5d6d03d
Nagarei <Nagarei@users.noreply.github.com>
2023-10-21 16:01:20 +0900

    fix update

--- webapp/sql/04_passwordupdate.sql
+++ webapp/sql/04_passwordupdate.sql
@@ -1,4 +1,3 @@
 UPDATE `users`
-SET    `users`.`hashed_password` = `password_table`.`weak_hashed_password`
-FROM   `users`
-  INNER JOIN `password_table` ON `users`.`hashed_password` = `password_table`.`hashed_password`;
+  INNER JOIN `password_table` ON `users`.`hashed_password` = `password_table`.`hashed_password`
+  SET    `users`.`hashed_password` = `password_table`.`weak_hashed_password`;
