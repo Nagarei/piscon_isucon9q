0b7e876324342d1882596bb48ad5b65a9700997d
Nagarei <Nagarei@users.noreply.github.com>
2023-10-21 22:17:02 +0900

    CREATE TABLE時に作っておく

--- webapp/sql/04_passwordupdate.sql
+++ webapp/sql/04_passwordupdate.sql
@@ -2,8 +2,6 @@ UPDATE `users`
   INNER JOIN `password_table` ON `users`.`hashed_password` = `password_table`.`hashed_password`
   SET    `users`.`hashed_password` = `password_table`.`weak_hashed_password`;
 
-ALTER TABLE `items` ADD `parent_category_id` int unsigned;
 UPDATE `items`
   JOIN `categories` ON `items`.`category_id` = `categories`.`id`
   SET    `items`.`parent_category_id` = `categories`.`parent_id`;
-ALTER TABLE items ADD INDEX idx_pcid_createdat_id (`parent_category_id`, `created_at`, `id`);
